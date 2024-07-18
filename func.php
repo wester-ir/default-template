eb346cdb57b5f41e7100479784be6e756791d644
Hossein Zare <zarehossein.hz1997@gmail.com>
2024-07-18 05:34:34 +0330

    Delete the func.php file.

--- func.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (! function_exists('templatePaginationView')) {
-    function templatePaginationView(): string
-    {
-        return 'templates.default.views.vendor.pagination.tailwind';
-    }
-}
