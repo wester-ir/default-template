3530d3ccf73765eec462be573c56c636ca511a2c
Hossein Zare <zarehossein.hz1997@gmail.com>
2024-10-11 15:48:44 +0330

    HTML Improvements.

--- views/pages/account/index.blade.php
+++ views/pages/account/index.blade.php
@@ -17,28 +17,28 @@
                         <a href="{{ route('client.account.orders.index') }}" class="font-light text-sm">همه سفارش ها</a>
                     </div>
 
-                    <div class="grid grid-cols-2 md:grid-cols-4 gap-x-5 gap-y-8 mt-8">
+                    <div class="grid grid-cols-2 lg:grid-cols-4 gap-x-5 gap-y-8 mt-8">
                         <a href="{{ route('client.account.orders.index', ['type' => 'pending']) }}" class="flex flex-col items-center justify-center">
                             <img src="{{ template_asset('/assets/img/icons/invoice.svg') }}" width="50" height="50">
-                            <div class="mt-3">در انتظار پرداخت</div>
+                            <div class="text-sm lg:text-base mt-3">در انتظار پرداخت</div>
                             <span class="badge badge-light mt-3">{{ $orderStats->pending }}</span>
                         </a>
 
                         <a href="{{ route('client.account.orders.index', ['type' => 'paid']) }}" class="flex flex-col items-center justify-center">
                             <img src="{{ template_asset('/assets/img/icons/cart-colorful.svg') }}" width="50" height="50">
-                            <div class="mt-3">در حال پردازش</div>
+                            <div class="text-sm lg:text-base mt-3">در حال پردازش</div>
                             <span class="badge badge-warning mt-3">{{ $orderStats->paid }}</span>
                         </a>
 
                         <a href="{{ route('client.account.orders.index', ['type' => 'shipped']) }}" class="flex flex-col items-center justify-center">
                             <img src="{{ template_asset('/assets/img/icons/order-shipped.svg') }}" width="50" height="50">
-                            <div class="mt-3">ارسال شده</div>
+                            <div class="text-sm lg:text-base mt-3">ارسال شده</div>
                             <span class="badge badge-success mt-3">{{ $orderStats->shipped }}</span>
                         </a>
 
                         <a href="{{ route('client.account.orders.index', ['type' => 'returned']) }}" class="flex flex-col items-center justify-center">
                             <img src="{{ template_asset('/assets/img/icons/order-returned.svg') }}" width="50" height="50">
-                            <div class="mt-3">مرجوع شده</div>
+                            <div class="text-sm lg:text-base mt-3">مرجوع شده</div>
                             <span class="badge badge-light mt-3">{{ $orderStats->returned }}</span>
                         </a>
                     </div>
