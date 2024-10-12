c1d4f56a564926482722a88e1ca315566d677375
Hossein Zare <zarehossein.hz1997@gmail.com>
2024-10-12 14:41:47 +0330

    Add padding-left to the search input.

--- views/layouts/default/search.blade.php
+++ views/layouts/default/search.blade.php
@@ -1,4 +1,4 @@
 <form action="{{ route('client.search') }}" onsubmit="return this.search.value.length > 2;" method="GET" class="search mr-3">
-    <input type="text" class="default rounded-full z-10" name="search" value="{{ query('search') }}" maxlength="50" placeholder="جستجو...">
+    <input type="text" class="default rounded-full z-10 pl-11" name="search" value="{{ query('search') }}" maxlength="50" placeholder="جستجو...">
     <i class="fi fi-rr-search flex absolute left-0 ml-4 z-20"></i>
 </form>
