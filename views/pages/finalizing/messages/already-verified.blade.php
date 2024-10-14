075c933d3ac1923afeb399365a25bcacf00162c7
Hossein Zare <zarehossein.hz1997@gmail.com>
2024-10-15 01:26:39 +0330

    Add order id and hide the show order btn for guests.

--- views/pages/finalizing/messages/already-verified.blade.php
+++ views/pages/finalizing/messages/already-verified.blade.php
@@ -7,6 +7,10 @@
         <img src="{{ template_asset('assets/img/success.svg') }}" class="w-24 h-24 mb-4">
         <h2 class="text-green-600">قبلاً ثبت شده!</h2>
         <div>سفارش مورد نظر قبلاً تایید شده است.</div>
-        <a href="{{ route('client.account.orders.order.index', $order) }}" class="btn btn-success mt-3">مشاهده سفارش</a>
+        <div class="mt-2">کد سفارش: {{ $order->id }}</div>
+
+        @auth
+            <a href="{{ route('client.account.orders.order.index', $order) }}" class="btn btn-success mt-3">مشاهده سفارش</a>
+        @endauth
     </div>
 @endsection
