1daba2ce65633f4cbfbfb2d83a34f8e0f94ad29d
Hossein Zare <zarehossein.hz1997@gmail.com>
2024-10-14 23:23:41 +0330

    Fix a minor bug in product tabs.

--- views/components/product/content-tabs.blade.php
+++ views/components/product/content-tabs.blade.php
@@ -1,21 +1,35 @@
+@php
+    if ($product->attributes->isEmpty()) {
+        if ($product->content->full_description) {
+            $activeTab = 'full_description';
+        } elseif ($product->content->sizing) {
+            $activeTab = 'sizing';
+        } else {
+            $activeTab = null;
+        }
+    } else {
+        $activeTab = 'attributes';
+    }
+@endphp
+
 <div class="content-tabs">
     <ul>
         @if (! $product->attributes->isEmpty())
-            <li data-id="attributes" data-active="true">مشخصات</li>
+            <li data-id="attributes" data-active="{{ as_string($activeTab === 'attributes') }}">مشخصات</li>
         @endif
 
         @if ($product->content->full_description)
-            <li data-id="full_description" data-active="{{ ! $product->attributes ? 'true' : 'false' }}">توضیحات تکمیلی</li>
+            <li data-id="full_description" data-active="{{ as_string($activeTab === 'full_description') }}">توضیحات تکمیلی</li>
         @endif
 
         @if ($product->content->sizing)
-            <li data-id="sizing" data-active="{{ ! $product->attributes && ! $product->content->full_description ? 'true' : 'false' }}">سایز بندی</li>
+            <li data-id="sizing" data-active="{{ as_string($activeTab === 'sizing') }}">سایز بندی</li>
         @endif
     </ul>
 
     <!-- Attributes -->
     @if ($product->attributes)
-        <div class="tab-section" data-id="attributes" data-active="true">
+        <div class="tab-section" data-id="attributes" data-active="{{ as_string($activeTab === 'attributes') }}">
             <ul class="divide-y">
                 @foreach ($product->merged_attributes as $attribute)
                     <li class="flex">
@@ -33,14 +47,14 @@
 
     <!-- Full Description -->
     @if ($product->content->full_description)
-        <div class="tab-section leading-[29px] [&>p]:mt-2 [&>p:first-child]:mt-0" data-id="full_description" data-active="{{ ! $product->attributes ? 'true' : 'false' }}">
+        <div class="tab-section leading-[29px] [&>p]:mt-2 [&>p:first-child]:mt-0" data-id="full_description" data-active="{{ as_string($activeTab === 'full_description') }}">
             {!! $product->content->full_description !!}
         </div>
     @endif
 
     <!-- Sizing -->
     @if ($product->content->sizing)
-        <div class="tab-section" data-id="sizing" data-active="{{ ! $product->attributes && ! $product->content->full_description ? 'true' : 'false' }}">
+        <div class="tab-section" data-id="sizing" data-active="{{ as_string($activeTab === 'sizing') }}">
             <table class="sizing-table">
                 <thead>
                     <tr>
