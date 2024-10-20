d2f82acbe5a2a0834e5bf0292d26fa65bb49715d
Hossein Zare <zarehossein.hz1997@gmail.com>
2024-10-20 22:03:56 +0330

    Remove a whitespace.

--- views/layouts/default.blade.php
+++ views/layouts/default.blade.php
@@ -106,7 +106,7 @@
                         <div class="overflow-x-auto hide-scrollbar flex-1">
                             <ul class="flex items-center space-x-4 space-x-reverse text-sm">
                                 <li class="navbar-indicator-trigger">
-                                    <a href="{{ route('client.latest') }}" class="whitespace-pre">جدید ترین ها</a>
+                                    <a href="{{ route('client.latest') }}" class="whitespace-pre">جدیدترین ها</a>
                                 </li>
                                 <li class="navbar-indicator-trigger">
                                     <a href="{{ route('client.best-selling') }}" class="whitespace-pre">پرفروش ترین ها</a>
