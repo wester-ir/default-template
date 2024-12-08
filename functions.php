6055db8bf91df3581a20cf7a86b0b2f39258766c
Hossein Zare <56504893+hossein-zare@users.noreply.github.com>
2024-12-08 14:15:06 +0330

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,13 +1,1 @@
 <?php
-
-use App\Core\Client\ProductCore;
-
-function client_index_func()
-{
-    $latestProducts = ProductCore::paginateLatestProducts(10);
-    $bestSellingProducts = ProductCore::paginateBestSellingProducts(10);
-
-    // Share data
-    \View::share('latestProducts', $latestProducts);
-    \View::share('bestSellingProducts', $bestSellingProducts);
-}
